958135b3e082b2ef8628791849ebd86d096138ba
Johachim <joachimgul@yahoo.dk>
2019-08-25 17:01:33 +0200

    Jet og 3rd person mangler ikke færdigt

---
+++ Assignment2/Assets/GrassRoadRace/Script/BoxBreak.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoxBreak : MonoBehaviour
+{
+    
+    int pointCounter = 0;
+    public AudioSource breakSource;
+    public Text t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        breakSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollision(Collision col)
+    {
+        if(col.gameObject.tag == "Box")
+        {
+        breakSource.Play();
+        Destroy(col.gameObject);
+        pointCounter++;
+        }
+            t.text = "Score: " + pointCounter;
+        
+    }
+}
---
+++ Assignment2/Assets/Scripts/Shooting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+
+    public GameObject bullet;
+    public float speed = 100f;
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject inBullet = Instantiate(bullet, transform.position, Quaternion.identity) as GameObject;
+            Rigidbody inBulletRigid = inBullet.GetComponent<Rigidbody>();
+            inBulletRigid.AddForce(Vector3.forward * speed);
+        }
+    }
+}
